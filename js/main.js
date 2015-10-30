9c1971f61b1f9eac92b54bfbb3744aabfd551a61
Tobi Kehinde <tokehinde11@gmail.com>
2015-10-29 21:47:05 -0600

    created app and added its controllers

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var app = angular.module('playerApp',[]);
+
+app.controller('PlayerController', ['$scope', function($scope){
+
+}]);
+
+app.controller('RelatedController', ['$scope', function($scope){
+
+}]);
