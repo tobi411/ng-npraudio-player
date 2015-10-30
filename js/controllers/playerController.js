92c6dd6d297f925ff73d2df5149edfe99b34bd20
Tobi Kehinde <tokehinde11@gmail.com>
2015-10-30 16:08:32 -0600

    created controller js file, improved stop functiolaity and made styling modifications

---
+++ js/controllers/playerController.js
@@ -0,0 +1,54 @@
+app.controller('PlayerController', ['$scope', '$http', function($scope,$http){
+	var apiKey = 'MDIxMDM5NDE2MDE0NDYxODQwODY2ZTllNg000';
+	var nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+	$scope.playing = false;
+	$scope.audio = document.createElement('audio');
+	$scope.shows = [];
+		$scope.play = function(program){
+		if($scope.playing)
+		{
+			$scope.audio.pause();
+			$scope.playing = false;
+			program.show_audio.pause;
+			program.show_playing = false;
+		}else{
+			var url = program.show_audio.audio[0].format.mp4.$text;
+			$scope.audio.src = url;
+			$scope.audio.play();
+			$scope.playing = true;
+			program.show_playing = true;
+		}		
+	};
+	$scope.stop = function(program){
+		// if(program.show_audio)
+		var url = program.show_audio.audio[0].format.mp4.$text;
+		if(url === $scope.audio.src)
+		{
+			$scope.audio.pause();
+			$scope.playing = false;
+			program.show_playing = false;
+		}		
+	};
+	$scope.audio.addEventListener('ended', function(){
+		$scope.$apply(function(){
+			$scope.stop();
+		});
+	});
+
+	$http({
+		method: 'JSONP',
+		url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+	}).success(function(data, status){
+		//store NPR response object
+		 var programs = data.list.story;
+		 for(var i = 0; i < programs.length; i++)
+		 {
+		 	$scope.shows[i] = {
+		 		show_audio: programs[i], 
+		 		show_playing: $scope.playing
+		 	};
+		 }
+	}).error(function(data, status){
+
+	});
+}]);
