d8ff2034bb66b7f7449c99bd882e6a9639a6a75d
Tobi Kehinde <tokehinde11@gmail.com>
2015-10-29 23:34:10 -0600

    added placeholder sound byte. Created play and stop methods in Player controller

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 var app = angular.module('playerApp',[]);
 
 app.controller('PlayerController', ['$scope', function($scope){
-
+	$scope.playing = false;
+	$scope.audio = document.createElement('audio');
+	$scope.audio.src = './media/cartman-InGhetto.mp3';
+	$scope.play = function(){
+		$scope.audio.play();
+		$scope.playing = true;
+	};
+	$scope.stop = function(){
+		$scope.audio.pause();
+		$scope.playing = false;
+	};
+	$scope.audio.addEventListener('ended', function(){
+		$scope.$apply(function(){
+			console.log("stopping");
+			$scope.stop();
+		});
+	});
 }]);
 
 app.controller('RelatedController', ['$scope', function($scope){
